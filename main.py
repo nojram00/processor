80bc96c6941156d67bc0f5d83ea4700ee5251e52
Nojram00 <petmalu.marjon@gmail.com>
2024-11-09 00:04:18 +0800

    initial

---
+++ main.py
@@ -0,0 +1,65 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+import json
+
+
+HOST_URL = "https://my-fucking-service.onrender.com"
+
+
+def fetch_and_modify_video():
+    # fetch data (Get only view count):
+    try:
+        response = requests.get(f"{HOST_URL}/get-video-info")
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        if not data["items"]:
+            print("No Video Found...")
+            return
+
+        info = data['items'][0]
+
+        view_count = info['statistics']['viewCount']
+
+        print(f"This video has {view_count} view/s")
+
+        update_video_title(f"This video has {view_count} views")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def update_video_title(title : str):
+
+    data = {
+            "title" : title,
+            "description" : "No Description kase tamad ako",
+            "tags" : "None"
+        }
+    try:
+        response = requests.post(f"{HOST_URL}/update-video", json=data)
+
+        response.raise_for_status()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+print("Running....")
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(fetch_and_modify_video, 'interval', hours=12)
+
+scheduler.start()
+
+try:
+    while True:
+        pass
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
+
+
